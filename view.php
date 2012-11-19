eee414c7d5a7c198ba2bcd3ad177a04595d5d1e6
jeonghwan <ej88ej@gmail.com>
2012-11-19 20:15:08 +0900

    view file adding

---
+++ view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <title>Bible Recoder</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <script type="text/javascript">
+    </script>
+</head>
+<body>
+<div id="wrapper">
+    <div id="top">
+        <h1><a href="index2.php">Bible Recoder</a></h1>
+    </div>
+    
+    <div id="middle">
+    <h3>구약성경 - 이사야 3장</h3>
+    <span id="verse">1.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+    <span id="verse">2.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+    <span id="verse">3.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+    <span id="verse">1.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+    <span id="verse">2.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+    <span id="verse">3.</span>
+    <p>웃시야의 손자이며, 요담의아들인 아하스가 유다 왕으로 있을 때에, 아람왕 르신과 르말리야의 아들인 이스라엘왕 베가가 예루살렘을 치러 올라왔습니다. 그러나 ㄱ들은 예루살렘을 무너뜨리지 못했습니다.</p>
+
+    <div id="list"><< 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 >></div>
+    </div> <!-- end of middle -->
+    
+    <div id="footer">
+    </div>
+</div> <!-- end of wrapper -->
+</body>
+</html>
